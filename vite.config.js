e068c09483a28d4b6b82b551858c807c795342a3
dima <dima@my-ubunty.myguest.virtualbox.org>
2024-10-05 23:18:37 +0300

    add: profile page

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
 import { resolve } from "path";
 import handlebars from "vite-plugin-handlebars";
+import { readdirSync } from "fs";
+
+const allTemplateDirectories = [];
+
+const getDirectories = (source, result) => {
+  const dir = readdirSync(source, { withFileTypes: true }).filter(
+    (dirent) => dirent.isDirectory()
+  );
+
+  dir.forEach((d) => {
+    result.push(resolve(__dirname, source, d.name));
+    getDirectories(`${source}/${d.name}`, result);
+  });
+};
+
+getDirectories(resolve(__dirname, "src/partials"), allTemplateDirectories)
 
 export default {
   plugins: [
     handlebars({
-      partialDirectory: [
-        resolve(__dirname, "src/partials/components"),
-        resolve(__dirname, "src/partials/pages"),
-        resolve(__dirname, "src/partials/layouts"),
-      ],
+      partialDirectory: allTemplateDirectories,
     }),
   ],
 };
