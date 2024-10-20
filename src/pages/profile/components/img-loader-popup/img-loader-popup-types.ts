d246ce7d03ef2af7ac72b8efcb68b1e0abead925
dima <dima@my-ubunty.myguest.virtualbox.org>
2024-10-20 20:30:42 +0300

    add: imgPopup

--- src/pages/profile/components/img-loader-popup/img-loader-popup-types.ts
+++ src/pages/profile/components/img-loader-popup/img-loader-popup-types.ts
@@ -1,5 +1,18 @@
-import { Button } from "../../../../components"
+import { Button, Label } from "../../../../components";
+import { IBlock, TProps } from "../../../../modules";
+
+export type TImgLoaderForm = {
+  submitButton: Button;
+  exitButton: Button;
+  errorLabel: Label;
+  fileLabel: Label;
+};
 
 export type TImgLoader = {
-    submitButton : Button
-}
+  content: IBlock<Record<string, TProps>>;
+};
+
+export type TImgName = {
+  okButton: Button;
+  fileName: string;
+};
